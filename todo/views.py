591a5b768c8b4f78538a5f163d9e600fdedd74d6
fidha-na <fidha@cybersquare.org>
2024-02-01 16:44:00 +0300

    remaining task function

--- todo/views.py
+++ todo/views.py
@@ -3,8 +3,10 @@ from todoapp.models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by('updated_at')
-    print(tasks)
+    completed_tasks = Task.objects.filter(is_completed=True)
+    print(completed_tasks)
     context={
-        'tasks': tasks
+        'tasks': tasks,
+        'completed_tasks':completed_tasks
     }
     return render(request,'home.html',context)
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,11 +1,48 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse
 from .models import Task
+from django.contrib import messages
 # Create your views here.
 
 def addTask(request):
-    taskName = (request.POST['task'])
+    taskName = (request.POST['task'].strip())
+    if(taskName == ""):
+        messages.error(request, 'Task name cannot be empty.')
+        return redirect('home')
     Task.objects.create(task=taskName)
     return redirect('home')
     
-    return HttpResponse('the form is submitted')
+def mark_as_done(request,pk):
+    task = get_object_or_404(Task,pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+def mark_as_undone(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = False
+    task.save()
+    return redirect('home')
+
+def edit_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if(request.method=='POST'):
+        new_task = request.POST['task']
+        task.task = new_task
+        task.save()
+        return redirect('home')
+    else:
+        context = {
+            'task':task
+        }
+        return render(request,'edit_task.html',context)
+
+def delete_task(request,pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
+    return redirect('home')
+
+    
+
+
+
